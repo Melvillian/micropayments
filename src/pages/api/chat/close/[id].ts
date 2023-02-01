b0e50702cf2244296d71dcff0d45df322934e42e
Rahul Patni <rahul20patni@gmail.com>
2023-01-31 22:07:01 -0800

    adding close and submit apis

---
+++ src/pages/api/chat/close/[id].ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "utils/prisma";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { id } = req.query;
+    const data = req.body.data;
+
+    if (!data) {
+      res.status(400).json({ error: "Missing data" });
+      return;
+    }
+
+    const paymentChannel = await prisma.paymentChannel.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!paymentChannel) {
+      res.status(404).json({ error: "Payment channel not found" });
+      return;
+    }
+
+    if (paymentChannel.closed) {
+      res.status(400).json({ error: "Payment channel already closed" });
+      return;
+    }
+
+    // TODO: logic here to verify data
+
+    const updatedPaymentChannel = await prisma.paymentChannel.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        closed: true,
+      },
+    });
+
+    res.status(200).json(updatedPaymentChannel);
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
