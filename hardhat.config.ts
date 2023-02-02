fb9ae95d8742558abf86833639c216cb9d7e0510
Melvillian <melvillevt@gmail.com>
2023-02-01 23:07:28 -0500

    add optimism rpc

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.17",
@@ -16,6 +20,11 @@ const config: HardhatUserConfig = {
   networks: {
     arbitrum: {
       url: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_ARBITRUM_API_KEY}`,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    optimism: {
+      url: `https://opt-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_OPTIMISM_API_KEY}`,
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
